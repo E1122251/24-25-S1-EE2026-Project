86f5c63d2065ef76b1763f4cd98bed667cf54b44
wngud9914 <wngud9914@gmail.com>
2024-10-27 15:09:53 +0800

    Update is_collision.v
    Added debouncing which solve random endscreen pop-ups

--- Sources/is_collision.v
+++ Sources/is_collision.v
@@ -21,16 +21,29 @@
 
 
 module is_collision(
+    input clock_100mhz,
     input is_player_hitbox,
     input is_obstacle_hitbox,
+    input game_active,
     
-    output reg is_collision);
-    always @ (*) begin
-        if ( is_player_hitbox && is_obstacle_hitbox ) begin
-            is_collision <= 1;
-        end
+    output reg is_collision=0);
+    
+    reg [3:0] counter = 0; //collision_debounce
+    always @ (posedge clock_100mhz) begin
+        if (game_active == 0)begin
+            is_collision <=0;
+            counter <=0;
+            end
+        else if ( is_player_hitbox && is_obstacle_hitbox ) begin
+            if (counter == 4'b1111) begin
+                is_collision <= 1;
+                end
+            else begin
+                counter <= counter +1;
+            end
+            end
         else begin
-            is_collision <= 0;
-        end
+            counter <= 0;         
+            end
     end
 endmodule
